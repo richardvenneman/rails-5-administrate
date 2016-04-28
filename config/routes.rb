70416c9ee3d73f843e6905460b5130b5ffe20ce9
Richard Venneman <richardvenneman@me.com>
2016-04-28 10:07:32 +0200

    Install Administrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :bookings
+
+    root to: "bookings#index"
+  end
+
   resources :bookings
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
